3cd6a4c6d19aaa3787d42fc139c32f4d580a70ab
Badshahpk <Piyushkulkarni48@gmail.com>
2018-02-02 19:24:06 +0530

    make for a106

--- lineage.mk
+++ lineage.mk
@@ -5,24 +5,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Call device specific makefile
-$(call inherit-product, device/highscreen/giraffe/cm_giraffe.mk)
+$(call inherit-product, device/micromax/a106/cm_giraffe.mk)
 
-LOCAL_PATH := device/highscreen/giraffe
+LOCAL_PATH := device/micromax/a106
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0.1/MMB29M/2419427:user/release-keys PRIVATE_BUILD_DESC="giraffe-user 6.0.1 MMB29M 2419427 release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="giraffe"
+    PRODUCT_DEVICE="a106"
 
 # IO Scheduler
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=bfq    
        
-PRODUCT_NAME := cm_giraffe
-PRODUCT_DEVICE := giraffe
-PRODUCT_BRAND := Highscreen
+PRODUCT_NAME := lineage_a106
+PRODUCT_DEVICE := a106
+PRODUCT_BRAND := micromax
 PRODUCT_MANUFACTURER := Mediatek
-PRODUCT_MODEL := Zera F
+PRODUCT_MODEL := a106
 
 # Correct bootanimation size for the screen
 TARGET_SCREEN_HEIGHT := 800
@@ -34,7 +34,7 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Inherit some common CM stuff.
 
-CM_BUILD := giraffe
+CM_BUILD := a106
 
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
